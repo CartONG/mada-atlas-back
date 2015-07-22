c5801b8be07f939f202b4ec9d540591ae69b3c02
madageomapping <madageomapping@users.noreply.github.com>
2015-07-22 13:00:51 +0200

    Update urls.py

--- ong/urls.py
+++ ong/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
     url(r'^actions/$', views.get_actions, name='actions'),
+    url(r'^actions/(?P<title>\w+)$', views.get_actions_by_title),
 )
