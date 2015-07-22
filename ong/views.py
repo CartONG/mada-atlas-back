766acfada35bd00348d93f282e092fb7a9569835
madageomapping <madageomapping@users.noreply.github.com>
2015-07-22 12:23:54 +0200

    Update views.py

--- ong/views.py
+++ ong/views.py
@@ -1,6 +1,9 @@
 #from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import RequestContext, loader
+from django.core import serializers 
+
+from ong.models import actions
 
 # Create your views here.
 
@@ -8,6 +11,13 @@ def home(request):
     template = loader.get_template('ong/base.html')
     #return HttpResponse(templates/ong/base.html)
     return HttpResponse(template.render())
+
+def get_actions(request):
+    actions = actions.objects.all()
+    data = serializers.serialize('json', actions)
+    return JsonResponse(data)
+    
+    
 
 #def home(request):
 #    return redirect('http://cartong.github.io/mada-front/dist/atlas/index.html', permanent=True)
