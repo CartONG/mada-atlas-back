cd031f642afdc4cd30bfc01042148912470e668b
madageomapping <madageomapping@users.noreply.github.com>
2015-07-22 12:39:53 +0200

    Update views.py

--- ong/views.py
+++ ong/views.py
@@ -1,7 +1,7 @@
 #from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from django.template import RequestContext, loader
-from django.core import serializers 
+#from django.core import serializers 
 
 from ong.models import action
 
@@ -14,8 +14,8 @@ def home(request):
 
 def get_actions(request):
     actions = action.objects.all()
-    data = serializers.serialize('json', actions)
-    return JsonResponse(data)
+#    data = serializers.serialize('json', actions)
+    return JsonResponse(actions)
     
     
 
