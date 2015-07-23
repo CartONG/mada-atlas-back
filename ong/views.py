5cadfa8e1efe490715db3db3229a132c65965005
madageomapping <madageomapping@users.noreply.github.com>
2015-07-23 14:35:00 +0200

    Update views.py

--- ong/views.py
+++ ong/views.py
@@ -9,6 +9,8 @@ from ong.models import action
 
 # Create your views here.
 
+#def home(request):
+#    return redirect('http://cartong.github.io/mada-front/dist/atlas/index.html', permanent=True)
 def home(request):
     template = loader.get_template('ong/base.html')
     #return HttpResponse(templates/ong/base.html)
@@ -29,16 +31,14 @@ def get_geoactions(request):
     data = serialize('geojson', actions , geometry_field = 'geom' , fields = ( 'titre' , 'description' , 'organisme' , 'categories' ,))
     return JsonResponse(data, safe=False)
 
-def api_action(request, id):
-    if request.method == 'GET':
-        action = Action.objects.get(pk=id)
-        data = serialize('json', action)
-    return JsonResponse(data)
-    elif request.method == 'POST':
-        action = Action.objects.get(pk=id)
-        action.title = request.post.title
-        action.description = request.post.description
-        action.save()
-    return JsonReponse('OK')    
-#def home(request):
-#    return redirect('http://cartong.github.io/mada-front/dist/atlas/index.html', permanent=True)
+#def api_action(request, id):
+#    if request.method == 'GET':
+#        action = Action.objects.get(pk=id)
+#        data = serialize('json', action)
+#    return JsonResponse(data)
+#    elif request.method == 'POST':
+#        action = Action.objects.get(pk=id)
+#        action.title = request.post.title
+#        action.description = request.post.description
+#        action.save()
+#    return JsonReponse('OK')    
