4e0bed98e7abc01dc96493fc2ab1683a021c3580
madageomapping <madageomapping@users.noreply.github.com>
2015-07-25 14:15:36 +0200

    Update settings.py

--- atlas/settings.py
+++ atlas/settings.py
@@ -120,7 +120,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(ROOT_DIR, 'static')
+STATIC_ROOT = os.path.join(ROOT_DIR, '../static')
 
 # GEOJSON serializer
 
