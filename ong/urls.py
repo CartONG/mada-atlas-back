378d41db98a1f68608286cb7666926cc8cbc0016
madageomapping <madageomapping@users.noreply.github.com>
2015-07-25 11:51:00 +0200

    Update urls.py

--- ong/urls.py
+++ ong/urls.py
@@ -3,10 +3,6 @@
 from django.conf.urls import patterns, include, url
 from . import views
 
-## DEBUG MODE ##
-from django.contrib.staticfiles import views
-################
-
 urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
     url(r'^actions/$', views.get_actions, name='actions'),
