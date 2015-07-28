99af8542732f3786fb7637088607c5f7325f2bf4
madageomapping <madageomapping@users.noreply.github.com>
2015-07-28 13:05:05 +0200

    Update models.py

--- ong/models.py
+++ ong/models.py
@@ -82,7 +82,7 @@ class action(models.Model):
     responsable = models.ForeignKey(utilisateur, limit_choices_to={'is_responsable': True}, verbose_name="nom du responsable de la fiche", to_field='user')
     organisme = models.ForeignKey(organisme, verbose_name="organisme maitre d'oeuvre", to_field='nom')
     avancement = models.ForeignKey('avancement', verbose_name="état d'avancement", to_field='nom') # Un "avancement" peut concerner plrs "actions" mais une "action" ne peut avoir qu'un état d'"avancement" => ForeignKey
-    categories = models.ManyToManyField('categorie', verbose_name="catégorie", to_field='nom') # Un ou plrs "catégories" peut qualifier une "action" et une "action" peut agir dans un ou plrs "catégories"  => ManyToManyField
+    categories = models.ManyToManyField('categorie', verbose_name="catégorie") # Un ou plrs "catégories" peut qualifier une "action" et une "action" peut agir dans un ou plrs "catégories"  => ManyToManyField
     creation = models.DateTimeField("Date de création fiche", auto_now_add=True)
     maj = models.DateTimeField("Date de mise à jour fiche", auto_now_add=True)
     geom = gismodels.PointField(srid=3857,default='SRID=3857;POINT(0.0 0.0)')
