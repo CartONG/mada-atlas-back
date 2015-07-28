0b3b10fd384614c59728f88bffd2564312352f8b
madageomapping <madageomapping@users.noreply.github.com>
2015-07-28 14:35:34 +0200

    Update models.py

--- ong/models.py
+++ ong/models.py
@@ -81,7 +81,7 @@ class action(models.Model):
     illustration = models.ImageField(upload_to="static/media/illustration/%Y/%m", blank=True, null=True)
     responsable = models.ForeignKey(utilisateur, limit_choices_to={'is_responsable': True}, verbose_name="nom du responsable de la fiche", to_field='user')
     organisme = models.ForeignKey(organisme, verbose_name="organisme maitre d'oeuvre")
-    avancement = models.ForeignKey('avancement', verbose_name="état d'avancement", default='') # Un "avancement" peut concerner plrs "actions" mais une "action" ne peut avoir qu'un état d'"avancement" => ForeignKey
+    avancement = models.ForeignKey('avancement', verbose_name="état d'avancement", default='En attente') # Un "avancement" peut concerner plrs "actions" mais une "action" ne peut avoir qu'un état d'"avancement" => ForeignKey
     categories = models.ManyToManyField('categorie', verbose_name="catégorie") # Un ou plrs "catégories" peut qualifier une "action" et une "action" peut agir dans un ou plrs "catégories"  => ManyToManyField
     creation = models.DateTimeField("Date de création fiche", auto_now_add=True)
     maj = models.DateTimeField("Date de mise à jour fiche", auto_now_add=True)
