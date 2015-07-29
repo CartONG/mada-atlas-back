c5f30ce1f2b775d4f482e8a5a9255d3fac151337
Thierry <herizo.razanajao@gmail.com>
2015-07-30 00:51:03 +0200

    Update 1 items

--- ong/models.py
+++ ong/models.py
@@ -82,7 +82,7 @@ class action(models.Model):
     responsable = models.OneToOneField(utilisateur, limit_choices_to={'is_responsable': True}, verbose_name="nom du responsable de la fiche", to_field='username')
     organisme = models.OneToOneField(organisme, verbose_name="organisme maitre d'oeuvre", to_field='nom')
     avancement = models.OneToOneField('avancement', verbose_name="état d'avancement", to_field='nom') 
-    categories = models.ManyToManyField('categorie', verbose_name="catégorie", through='categorie') # Une ou plrs "catégories" peut qualifier une "action" et une "action" peut agir dans un ou plrs "catégories"  => ManyToManyField
+    categories = models.ManyToManyField('categorie', verbose_name="catégorie") # Une ou plrs "catégories" peut qualifier une "action" et une "action" peut agir dans un ou plrs "catégories"  => ManyToManyField
     creation = models.DateTimeField("Date de création fiche", auto_now_add=True)
     maj = models.DateTimeField("Date de mise à jour fiche", auto_now_add=True)
     geom = gismodels.PointField(srid=3857,default='SRID=3857;POINT(0.0 0.0)')
