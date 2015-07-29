774e98dd5585436d23d15a222df4952859de48e2
Thierry <herizo.razanajao@gmail.com>
2015-07-30 01:48:37 +0200

    Update 1 items

--- ong/models.py
+++ ong/models.py
@@ -52,8 +52,9 @@ class status(models.Model):
         return u"Status: %s" % self.nom
 
    
-class utilisateur(models.Model):
-    user = models.OneToOneField(User.username) # La liaison OneToOne vers le modèle User (mail-nom-prenom-password)
+class utilisateur(AbstractBaseUser):
+    user = models.CharField(max_length=40, unique=True) # La liaison OneToOne vers le modèle User (mail-nom-prenom-password)
+    USERNAME_FIELD = 'user'
     photo = models.ImageField(upload_to="static/media/photos/%Y/%m", blank=True, null=True)
     organisme = models.ForeignKey(organisme) # Va servir plus tard de groupe pour inclure les "users"
     is_responsable = models.BooleanField("Responsable autorisé à éditer la fiche", default=False)
@@ -79,7 +80,7 @@ class action(models.Model):
     description = models.TextField(null=True)
     localisation = models.CharField(max_length=50)
     illustration = models.ImageField(upload_to="static/media/illustration/%Y/%m", blank=True, null=True)
-    responsable = models.OneToOneField(utilisateur, limit_choices_to={'is_responsable': True}, verbose_name="nom du responsable de la fiche")
+    responsable = models.ForeignKey(utilisateur, limit_choices_to={'is_responsable': True}, verbose_name="nom du responsable de la fiche", to_field='user')
     organisme = models.OneToOneField(organisme, verbose_name="organisme maitre d'oeuvre", to_field='nom')
     avancement = models.OneToOneField('avancement', verbose_name="état d'avancement", to_field='nom') 
     categories = models.ManyToManyField('categorie', verbose_name="catégorie") # Une ou plrs "catégories" peut qualifier une "action" et une "action" peut agir dans un ou plrs "catégories"  => ManyToManyField
