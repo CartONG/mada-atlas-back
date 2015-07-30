4ac888668d90d723300502013759a6c7d9cb4642
Thierry <herizo.razanajao@gmail.com>
2015-07-30 02:49:39 +0200

    Update 1 items

--- ong/models.py
+++ ong/models.py
@@ -64,8 +64,7 @@ class utilisateur(models.Model):
         verbose_name_plural = "utilisateurs"
         ordering = ['user']
 
-    def __unicode__(self):
-        return u"User: %s" % self.user
+
 
     def appartenance(self):
         "Returns the organisme and person's full name."
