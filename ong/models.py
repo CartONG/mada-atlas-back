26e8ce3e685196cbe9416bbbe4b113064b360e2d
Thierry <herizo.razanajao@gmail.com>
2015-07-30 03:00:42 +0200

    Update 1 items

--- ong/models.py
+++ ong/models.py
@@ -53,7 +53,8 @@ class status(models.Model):
 
    
 class utilisateur(models.Model):
-    user = models.OneToOneField(User) # La liaison OneToOne vers le modèle User (mail-nom-prenom-password)
+    user = models.OneToOneField(User, db_column='auth_user_id',
+        primary_key=True, parent_link=True) # La liaison OneToOne vers le modèle User (mail-nom-prenom-password)
     photo = models.ImageField(upload_to="static/media/photos/%Y/%m", blank=True, null=True)
     organisme = models.ForeignKey(organisme) # Va servir plus tard de groupe pour inclure les "users"
     is_responsable = models.BooleanField("Responsable autorisé à éditer la fiche", default=False)
