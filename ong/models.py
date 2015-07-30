cd00f53eaf573192f8002d47516a0098a4a23eb9
Thierry <herizo.razanajao@gmail.com>
2015-07-30 02:37:55 +0200

    Update 1 items

--- ong/models.py
+++ ong/models.py
@@ -64,7 +64,7 @@ class utilisateur(models.Model):
         ordering = ['user']
 
     def __unicode__(self):
-        return u"User: %s" % self.user.username
+        return u"User: %s" % self.User.username
 
     def appartenance(self):
         "Returns the organisme and person's full name."
